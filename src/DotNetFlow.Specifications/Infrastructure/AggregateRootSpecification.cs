8e1c0e99548f245e4995662e7b2b12f8cc6e5542
Ben Smith <ben@dotnetflow.com>
2011-04-25 21:06:29 +0100

    Created ASP.NET MVC3 website (DotNetFlow) with class library for Core containing domain model and commands/events using NCQRS and Specifications library for tests.

---
+++ src/DotNetFlow.Specifications/Infrastructure/AggregateRootSpecification.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Ncqrs.Domain;
+using NUnit.Framework;
+using Ncqrs.Eventing.Sourcing;
+
+namespace DotNetFlow.Specifications.Infrastructure
+{
+    public abstract class AggregateRootSpecification<TAggregateRoot> where TAggregateRoot : AggregateRoot, new()
+    {
+        protected abstract IEnumerable<ISourcedEvent> Given();
+        protected abstract void When();
+
+        protected TAggregateRoot Subject;
+        protected Exception RaisedException;
+        protected IEnumerable<ISourcedEvent> Events;
+
+        [SetUp]
+        public void Setup()
+        {
+            Subject = new TAggregateRoot();
+            Subject.InitializeFromHistory(Given());
+
+            try
+            {
+                When();
+                Events = Subject.GetUncommittedEvents();
+            }
+            catch (Exception ex)
+            {
+                RaisedException = ex;
+            }
+        }
+    }
+}
---
+++ src/DotNetFlow/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetFlow.MvcApplication" Language="C#" %>
